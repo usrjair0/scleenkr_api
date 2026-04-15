732d057bcc338d64b7e472fa3b391da8f2b1c62e
Everson Developer <everson.dev@gmail.com>
2026-04-15 13:07:38 -0300

    chore: configurando gitignore e quebras de linha

--- configuracaoBanco.js
+++ configuracaoBanco.js
@@ -1,7 +1,7 @@
 const credenciais = {
     user: process.env.DB_USER || 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'everscash03',
+    database: process.env.DB_NAME || 'cleenkr',
     password: process.env.DB_PASSWORD || '48344834',
     port: process.env.DB_PORT || 5432,
 };
