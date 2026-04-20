692bcc552ac8fec9f7137c4625cce140ec6ec0ae
juniorLG-dev <contaproxadrez2023@gmail.com>
2026-04-20 18:54:11 -0300

    chore: create a dockerized environment for faster startup

--- configuracaoBanco.js
+++ configuracaoBanco.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const credenciais = {
     user: process.env.DB_USER || 'postgres',
     host: process.env.DB_HOST || 'localhost',
@@ -5,4 +6,5 @@ const credenciais = {
     password: process.env.DB_PASSWORD || '48344834',
     port: process.env.DB_PORT || 5432,
 };
+console.log(credenciais.password)
 module.exports = credenciais;
